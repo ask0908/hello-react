e47728e8ee6f53b165a8ed8709afbede68dac1e7
ask0908 <askdfjaslk@naver.com>
2022-04-11 00:49:57 +0900

    3장 컴포넌트 코드 테스트

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,20 +1,24 @@
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-    return <div>
-        하이여 내 이름은 {name}임 <br/>
-        children 값은 {children}임~ <br/>
-        내가 좋아하는 숫자는 {favoriteNumber}임!
-    </div>;
-};
-
-MyComponent.defaultProps = {
-    name: "기본 이름"
-}
-
-MyComponent.propTypes = {
-    name: PropTypes.string,
-    favoriteNumber: PropTypes.number.isRequired
+class MyComponent extends Component {
+    static defaultProps = {
+        name: '기본 이름'
+    };
+    static propTypes = {
+        name: PropTypes.string,
+        favoriteNumber: PropTypes.number.isRequired
+    };
+    render() {
+        const { name, favoriteNumber, children } = this.props;  // 비구조화 할당
+        return (
+            <div>
+                하이여 내 이름은 {name}입니다 <br/>
+                children 값은 {children}임~ <br/>
+                내가 좋아하는 숫자는 {favoriteNumber}임!
+            </div>
+        );
+    }
 }
 
 export default MyComponent;
